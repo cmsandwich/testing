475eba77509840cc2dcd9f3e091826aabc3ab7f9
cmsandwich <89049010+cmsandwich@users.noreply.github.com>
2021-10-21 21:38:55 -0400

    Update oostest.js

--- oostest.js
+++ oostest.js
@@ -55,7 +55,7 @@ window.gibinit = function() {
 
   function target_addToCart(DEBUG_NODE, UPC) {
     var cartUrl = 'https://carts.target.com/web_checkouts/v1/cart_items?field_groups=CART%2CCART_ITEMS%2CSUMMARY&key=feaf228eb2777fd3eee0fd5192ae7107d6224b39';
-    var cart = {"cart_type":"REGULAR","channel_id":"10","shopping_context":"DIGITAL","cart_item":{"tcin":UPC,"quantity":2,"item_channel_id":"10"},"fulfillment":{"fulfillment_test_mode":"grocery_opu_team_member_test"}};
+    var cart = {"cart_type":"REGULAR","channel_id":"10","shopping_context":"DIGITAL","cart_item":{"tcin":UPC,"quantity":1,"item_channel_id":"10"},"fulfillment":{"fulfillment_test_mode":"grocery_opu_team_member_test"}};
     
     DEBUG_NODE.innerText = 'Attempting Cart Add...';
 
@@ -208,7 +208,7 @@ window.gibinit = function() {
       window.gibSoundLoopSource = source;
 
       if (duration > 0) {
-        setTimeout(window.gibStopSound, (duration * 500));
+        setTimeout(window.gibStopSound, (duration * 1000));
       }
     }
 
@@ -296,11 +296,11 @@ window.gibinit = function() {
   };
 
   var gib = {
-    GIB_VERSION:    '2.2.7',
+    OOS_VERSION:    '2.1.0',
     PRODUCT_UPC:    detectUPC(),
     PRODUCT_TITLE:  detectProductName(),
 
-    refreshSeconds: window.gibRetailer == 'gamestop' ? 60 : 5,
+    refreshSeconds: window.gibRetailer == 'gamestop' ? 60 : 4,
 
     createElement: function(tag, id, styles) {
       var node = document.createElement(tag);
